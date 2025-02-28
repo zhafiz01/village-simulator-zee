8d6320a7f6d6343eb830e83672a97e422c7dec63
DeVonte Gooden <dg208804@gmail.com>
2025-02-27 20:17:59 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import Map from "./components/Map";
+import { useState } from 'react'
+
 
 const App = () => {
+  const [resources, setResources] = useState<Resource[]>([
+    {
+      type: "lumberMill",
+      amount: 5
+    },
+    {
+      type: "Grain",
+      amount: 5, 
+    },
+    {
+      type: "Water",
+      amount: 5, 
+    },
+    {
+      type: "Sheep",
+      amount: 1, 
+    },
+    {
+      type: "People",
+      amount: 0, 
+    },
+  ]);
+
+
   return (
     <div>
       <h1>Village Simulator</h1>
