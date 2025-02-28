b77079c2fc5eeab6c672cb31592ab1bbde94b6fe
Mara Regan <reganmarae@hotmail.com>
2025-02-27 20:14:11 -0500

    added placeholders for improvements which for now just cycle on-click

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,16 +1,22 @@
 import "../Tile.sass";
 
 interface TileProps {
-  index: number;
-  onClick: (index: number) => void;
+  index: number
+  improvement: "house" | "field" | "pasture" | "lumber-mill" | "well" | null
+  onClick: (index: number) => void
 }
 
-const Tile = ({ index, onClick }: TileProps) => {
+const Tile = ({ index, improvement, onClick }: TileProps) => {
   return (
-    <div className="tile" onClick={() => onClick(index)}>
-      
+    <div 
+      className={`tile ${improvement
+        ? `${improvement}-placeholder`
+        : ""}`} 
+      onClick={() => onClick(index)}
+    >
+      {improvement && <span className="improvement-label">{improvement}</span>}
     </div>
-  );
-};
+  )
+}
 
-export default Tile;
+export default Tile
